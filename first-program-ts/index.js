376965d42a7936c96bb7318bc35c6c59740d7f62
oldroge <rogeriomrjr@gmail.com>
2022-08-22 14:09:23 -0300

    study: compiling first program with typescript

---
+++ first-program-ts/index.js
@@ -0,0 +1,3 @@
+var idade = 30;
+idade = 'trinta';
+// Por inferência o typescript entende que o valor da variável 'idade' é 30, um number. No caso ele não permite que ao reatribuir a variável, seja com outro tipo, neste caso string.
